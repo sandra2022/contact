85ef6a9aa8adce4ba466106a79dcdfc3a15c5200
sandra2022 <mca544@rajagiri.edu>
2021-04-08 11:44:36 +0530

    edited

---
+++ resources/views/create.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">"
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+    <table class="table">
+    <tr>
+    <td>NAME</td>
+    <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+    <td>DESIGNATION</td>
+    <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+    <td>COLLEGE NAME</td>
+    <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+    <td>CONTACT NUMBER</td>
+    <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+    <td><button class="btn btn-secondary">SUBMIT</button></td>
+    </tr>
+  </table>
+  </div>
+  </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>"
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\controllername;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Routes::get("/",[facultycontrollername::class],'create');
+
+
